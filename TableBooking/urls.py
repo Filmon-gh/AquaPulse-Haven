a437e5c3a8833d1145b6a51066d6f0cde88868b3
Filmon Ghebremichael <filmo2020gh@gmail.com>
2023-07-29 12:54:46 +0000

    Add base.html and home.html templates for  home page content

--- TableBooking/urls.py
+++ TableBooking/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from TableBookingx.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
     path('accounts/', include('allauth.urls')),
+    
 ]
 
 
