79b73253aabcb32a8645edd06fa6d48093c187d2
Filmon Ghebremichael <filmo2020gh@gmail.com>
2023-08-01 15:37:45 +0000

    Add account detauls for template

--- TableBooking/settings.py
+++ TableBooking/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tablebooking-x-5fd9876f6880.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['tablebooking-x-5fd9876f6880.herokuapp.com', 'localhost', '8000-filmongh-tablebookingx-5qkjjjvm6qf.ws-eu102.gitpod.io']
 
 
 # Application definition
