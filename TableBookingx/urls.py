240ee45ca8b91cd206265cbc69b1d3c6160c1f94
Filmon Ghebremichael <filmo2020gh@gmail.com>
2023-09-10 13:44:25 +0000

    Add URL paths for updating, listing, and deleting reservations in urls.py

--- TableBookingx/urls.py
+++ TableBookingx/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('reservation/', views.reservation_form, name='reservation'),
     path('reservation/success/', views.reservation_success, name='reservation_success'),
-   
+    path('reservation/update/<int:reservation_id>/', views.update_reservation, name='update_reservation'),
+    path('reservations/', views.reservation_list, name='reservation_list'),
+    path('reservation/delete/<int:reservation_id>/', views.delete_reservation, name='delete_reservation'),
 ]
 
 
