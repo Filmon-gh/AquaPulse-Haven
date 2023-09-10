2914e424b3894cc77dcb90fe866f45752012c4dc
Filmon Ghebremichael <filmo2020gh@gmail.com>
2023-09-10 02:21:13 +0000

    Add URL path for reservation_success view

--- TableBookingx/urls.py
+++ TableBookingx/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('reservation/', views.reservation_form, name='reservation'),
+    path('reservation/success/', views.reservation_success, name='reservation_success'),
+   
 ]
 
 
