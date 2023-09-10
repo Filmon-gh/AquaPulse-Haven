3eb64beed22203a44482ea3dd855a4c8c8c1ff13
Filmon Ghebremichael <filmo2020gh@gmail.com>
2023-09-10 13:49:21 +0000

    Add 'reservation_form' to handle user-specific reservations and  'reservation_success' view for success messages

--- TableBookingx/views.py
+++ TableBookingx/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ReservationForm
+from .models import Reservation 
 # Create your views here.
 
 def home_view(request):
     return render(request, 'home.html')
+   
 
+@login_required
+def reservation_form(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            reservation.user = request.user
+            reservation.save()
+            return redirect('reservation_success')
+    else:
+        form = ReservationForm()
 
+    # Query reservations for the current user
+    reservations = Reservation.objects.filter(user=request.user)
+
+    return render(request, 'reservation_form.html', {'form': form, 'reservations': reservations})
     
+def reservation_success(request):
+    return render(request, 'reservation_success.html')
